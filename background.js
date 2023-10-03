a8a7b58fd83b9d1317a961369bb59b4e5c8130bf
josiascastro <josiascastro2003@gmail.com>
2023-10-03 10:26:36 -0300

    update background.js

--- background.js
+++ background.js
@@ -1,10 +1,13 @@
-chrome.webRequest.onHeadersReceived.addListener(
-  (details)=>{
-    if(details.url == "https://open.spotifycdn.com/cdn/build/web-player/web-player.34af1eea.js"){
-      let resultingUrl = 'javascript:'
-      return { redirectUrl: resultingUrl }
-    }
-  },
-  { urls: ["<all_urls>"] },
-  ["blocking", "responseHeaders"]
-);
+(function () {
+  chrome.webRequest.onHeadersReceived.addListener(
+    (details)=>{
+      const urlPattern = /^https:\/\/open\.spotifycdn\.com\/cdn\/build\/web-player\/web-player\.[a-f0-9]+\.js$/;
+      if (urlPattern.test(details.url)) {
+        let resultingUrl = 'javascript:';
+        return { redirectUrl: resultingUrl };
+      }
+    },
+    { urls: ["<all_urls>"] },
+    ["blocking", "responseHeaders"]
+  );
+})();
